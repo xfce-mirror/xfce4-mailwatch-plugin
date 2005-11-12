ccf877baffb78a6b73506733a7ff23de84f032d4
Pasi Orovuo <pasi.ov@gmail.com>
2005-11-12 13:25:34 +0000

    Move Gmail to top of the list to make it nicer.
    (Old svn revision: 1671)

--- panel-plugin/mailwatch.c
+++ panel-plugin/mailwatch.c
@@ -84,12 +84,12 @@ extern XfceMailwatchMailboxType builtin_mailbox_type_mh;
 extern XfceMailwatchMailboxType builtin_mailbox_type_gmail;
 
 XfceMailwatchMailboxType *builtin_mailbox_types[] = {
+    &builtin_mailbox_type_gmail,
     &builtin_mailbox_type_imap,
     &builtin_mailbox_type_pop3,
     &builtin_mailbox_type_maildir,
     &builtin_mailbox_type_mbox,
     &builtin_mailbox_type_mh,
-    &builtin_mailbox_type_gmail,
     NULL
 };
 #define N_BUILTIN_MAILBOX_TYPES (sizeof(builtin_mailbox_types)/sizeof(builtin_mailbox_types[0]))
